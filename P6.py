c07dc25003cbd4a24eca725b5d8c68578d034a03
Reece Bourgeois <reecebourgeois@gmail.com>
2021-11-30 13:16:19 -0600

    fixed to only show names in all files

--- P6.py
+++ P6.py
@@ -3,29 +3,39 @@ import sys
 
 # Name of zip file passed as a command line argument
 zipFileName = sys.argv[1]
-# Array of names iterated through
-names = []
 # Array of duplicate names
 duplicates = []
 
 # Open namelists.zip without extracting it
 with ZipFile(zipFileName,'r') as zip:
-    # For each file in the zipped archive
-    for file in zip.namelist():
-        # Open the text file
-        with zip.open(file) as textFile:
-            # For each line
-            for line in textFile:
-                # Isolate the name
-                nameOfPerson = line.decode('utf-8').strip()
-                # If the name is present in the names array, add the name to the duplicate array
-                if nameOfPerson in names:
-                    # If the name is already duplicated, don't add it again
-                    if nameOfPerson not in duplicates:
-                        duplicates.append(nameOfPerson)
-                # Otherwise, add the name to the names array
-                else:
-                    names.append(nameOfPerson)
+    # Open the first file in the zipped archive
+    with zip.open(zip.namelist()[0]) as firstFile:
+        # For each line
+        for line in firstFile:
+            # Isolate the name
+            nameOfPerson = line.decode('utf-8').strip()
+            # Not a duplicate by default
+            duplicate = False
+            # Loop through all names in the rest of the files
+            for file in zip.namelist()[1:]:
+                # Open the text file
+                with zip.open(file) as textFile:
+                    # The name is by default not a duplicate
+                    duplicate = False
+                    # For each line
+                    for line in textFile:
+                        # Isolate the name
+                        nameOfPerson2 = line.decode('utf-8').strip()
+                        # If the name is a duplicate, set to true
+                        if nameOfPerson2 == nameOfPerson:
+                            duplicate = True
+                    # If name is not a duplicate, break the loop
+                    if duplicate == False:
+                        break
+            # If duplicate is True, add the name to the duplicates array
+            if duplicate == True:
+                duplicates.append(nameOfPerson)
+            
 
 # Write duplicates to output.txt
 with open('output.txt','w') as output:
